4e9a742263895f4027f82f977942a3596c83e44b
Fjodor Zotaj <fjodorzotaj@outlook.com>
2016-02-14 14:25:49 -0500

    Changes to the template files and added authentication to detail view page

--- src/products/views.py
+++ src/products/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 from .models import Product
 
 def detail_view(request):
-	# GET 1 item
-	print request
-	product  = Product.objects.all().first()
-	template = "detail_view.html"
-	context = {
-		"product": product
-	}
-
+	# GET only 1 item
+	if request.user.is_authenticated():
+		print request
+		product  = Product.objects.all().first()
+		template = "detail_view.html"
+		context = {
+			"object": product
+		}
+	else:
+		template = "not_found.html"
+		context = {}
 	return render(request, template, context)
 
 def list_view(request):
