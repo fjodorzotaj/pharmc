c4d53c6c8421226c0e53835c0ad1bcc39e280da2
Fjodor Zotaj <fjodorzotaj@outlook.com>
2016-02-14 12:19:28 -0500

    Added extra field in admin product list and a search field

--- src/products/admin.py
+++ src/products/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ["__unicode__", "description", "price", "sale_price",]
+	search_fields = ["title", "description", "price",]
+	class Meta:
+		model = Product
+
+
+admin.site.register(Product, ProductAdmin)
