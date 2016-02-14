85625c0f8cf753e560809ceefef75f97bfce2dd8
Fjodor Zotaj <fjodorzotaj@outlook.com>
2016-02-14 14:18:36 -0500

    Mage changes to the view context and template files

--- src/products/views.py
+++ src/products/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 
+from .models import Product
+
 def detail_view(request):
 	# GET 1 item
 	print request
+	product  = Product.objects.all().first()
 	template = "detail_view.html"
-	context = {}
+	context = {
+		"product": product
+	}
 
 	return render(request, template, context)
 
 def list_view(request):
 	# GET a list of multiple items
 	print request
+	queryset = Product.objects.all()
 	template = "list_view.html"
-	context = {}
+	context = {
+		"queryset": queryset
+	}
 
 	return render(request, template, context)
