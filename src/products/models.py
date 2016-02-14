d67cc7792eb3875371aba552f5aea7e0cb197e76
Fjodor Zotaj <fjodorzotaj@outlook.com>
2016-02-14 11:06:54 -0500

    Created the new model for products

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Our model below
+
+class Product (models.Model):
+	title = models.CharField(max_length=140)
+
+	def __unicode__(self): # in python 3 instead of __unicode__ we use __str__(self)
+		return self.title
