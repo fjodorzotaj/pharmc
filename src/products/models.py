a0e8d959fde3e15c6693a89e02bba1c66a773cb6
Fjodor Zotaj <fjodorzotaj@outlook.com>
2016-02-14 11:55:44 -0500

    Added sales price to the poducts model

--- src/products/models.py
+++ src/products/models.py
@@ -6,6 +6,7 @@ class Product (models.Model):
 	title = models.CharField(max_length=140)
 	description = models.TextField()
 	price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99) #100.00
+	sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=6.99, null=True, blank=True)
 
 	def __unicode__(self): # in python 3 instead of __unicode__ we use __str__(self)
 		return self.title
